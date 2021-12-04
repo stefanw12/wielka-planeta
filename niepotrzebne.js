d800c03b87062968b472efc7d6a2bf3c99a97951
Stefan Wiśniewski <stefanwisniewski12@gmail.com>
2021-12-04 14:56:53 +0100

    dodawanie kodu do niepotrzebne.js

--- niepotrzebne.js
+++ niepotrzebne.js
@@ -1,5 +1,5 @@
 // W tym pliku przechowywany jest kod, który nie jest nigdzie potrzebny. \\
-
+//
 // $("body").bind("vmousedown", function(event) {
 //     let x = event.clientX;
 //     let y = event.clientY;
@@ -32,4 +32,41 @@
 //             kierunek = "dol";
 //         }
 //     }
+// }
+//
+// if (strona === "prawo") {
+//     plotnoCtx.beginPath()
+//     plotnoCtx.moveTo(x, y);
+//     let xTrojkata = x + 30
+//     plotnoCtx.lineTo(x + 30, y);
+//     plotnoCtx.stroke();
+//     plotnoCtx.beginPath();
+//     plotnoCtx.moveTo(xTrojkata, y)
+//     plotnoCtx.lineTo(xTrojkata, y + 10);
+//     plotnoCtx.lineTo(xTrojkata + 15, y);
+//     plotnoCtx.lineTo(xTrojkata, y - 10);
+//     plotnoCtx.lineTo(xTrojkata, y);
+//     plotnoCtx.fill();
+// } else if (strona === "lewo") {
+//     plotnoCtx.beginPath();
+//     plotnoCtx.moveTo(x, y);
+//     plotnoCtx.lineTo(x - 30, y)
+//     xTrojkata = x - 30;
+//     plotnoCtx.stroke();
+//     plotnoCtx.beginPath();
+//     plotnoCtx.moveTo(xTrojkata, y);
+//     plotnoCtx.lineTo(xTrojkata, y - 10);
+//     plotnoCtx.lineTo(xTrojkata - 15, y);
+//     plotnoCtx.lineTo(xTrojkata, y + 10);
+//     plotnoCtx.lineTo(xTrojkata, y);
+//     plotnoCtx.fill();
+// } else if (strona === "dol") {
+//     plotnoCtx.beginPath();
+//     plotnoCtx.moveTo(x, y);
+//     let yTrojkata = y + 30
+//     plotnoCtx.lineTo(x, y + 30);
+//     plotnoCtx.stroke();
+//     plotnoCtx.beginPath();
+//     plotnoCtx.moveTo(x, yTrojkata);
+//     plotnoCtx.fill();
 // }
